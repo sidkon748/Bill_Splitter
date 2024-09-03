7f0b3e0776db7cda38e98f9560dbfadfa5037227
Siddhartha Konduru <kondurus@usf.edu>
2024-09-03 17:55:58 -0400

    Initial logic for calculating tip using ternary operator

---
+++ cc_2.js
@@ -0,0 +1,2 @@
+let bill = 100; // Declare variable bill equal to a number
+let tip = (bill => 50 && bill <= 300) ? bill * .15 : bill * .2; // If variable bill is between 50 and 300, it's multiplied by .15, else it's multiplied by .2
