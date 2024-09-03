5ef5d60199c9ed060e03d82e94b504b363dbd13c
Siddhartha Konduru <kondurus@usf.edu>
2024-09-03 18:53:57 -0400

    Add output functionality to display bill, tip, and total

--- cc_2.js
+++ cc_2.js
@@ -1,2 +1,7 @@
+// Calculate the Tip
 let bill = 100; // Declare variable bill equal to a number
-let tip = (bill => 50 && bill <= 300) ? bill * .15 : bill * .2; // If variable bill is between 50 and 300, it's multiplied by .15, else it's multiplied by .2
+let tip = (bill => 50 && bill <= 300) ? bill * .15 : bill * .2; /* If variable bill is between 50 and 300, it's multiplied by .15, 
+else it's multiplied by .2*/
+
+//Output Details
+console.log("The bill was $" + bill +", the tip was $" + tip +", and the total value is $" + (tip + bill))
