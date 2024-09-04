d3fc59752b4dd945100056d2c9792f9b638af395
Siddhartha Konduru <kondurus@usf.edu>
2024-09-04 11:26:07 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -4,4 +4,14 @@ let tip = (bill => 50 && bill <= 300) ? bill * .15 : bill * .2; /* If variable b
 else it's multiplied by .2*/
 
 //Output Details
-console.log("The bill was $" + bill +", the tip was $" + tip +", and the total value is $" + (tip + bill))
+console.log("The bill was $" + bill +", the tip was $" + tip +", and the total value is $" + (tip + bill));
+
+//Create a Function calculateTip
+function calculateTip(billAmount) {
+    if (billAmount >= 50 && billAmount <= 300) {
+        return billAmount * 0.15; // 15% tip for bills between $50 and $300
+    } else {
+        return billAmount * 0.20; // 20% tip for any other amount
+    }
+}
+console.log(calculateTip(100));//Display tip amount with billAmount input as a number
